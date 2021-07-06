6bec2ea03913c28d98a1f4ff89abd44a71d4e482
suzuka-fukami <suzuka.fukami@gmail.com>
2021-07-06 14:02:43 +1000

    no message

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  Platform,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      //FlatList searches for key property automatically
+      { key: Math.random().toString(), value: enteredGoal },
+    ]);
+    // setCourseGoals([...courseGoals, enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Course Goal"
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <FlatList
+        style={styles.listContainer}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+
+      {/* ScrollView will make your app slow if the list is infinite because it will render all the items at once */}
+      {/* <ScrollView style={styles.listContainer}>
+        {courseGoals.map((goal) => 
+          <View style={styles.listItem}>
+            <Text key={goal}>{goal}</Text>
+          </View>
+        )}
+      </ScrollView> */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 6,
+    minWidth: 200,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  listContainer: {
+    margin: 20,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  screen: {
+    marginTop: Platform.OS === "ios" ? 50 : 50,
   },
 });
